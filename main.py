13d7c13b71f1c38ff7ab30c12b575133ebde978d
suitendaal <sven.uitendaal1@gmail.com>
2018-04-10 15:35:54 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import matlab.engine as me
+# import matlab.engine as me
+# from classes.hmiDrawer import HMIDrawer
+#
+#
+# if __name__ == '__main__':
+#     hmiDrawer = HMIDrawer()
+#     hmiDrawer.draw()
+
+from tkinter import *
+
+
+root = Tk()
+
+photo = PhotoImage(file='resources/platoontomtom_empty.png')
+label = Canvas(root, image=photo)
+label.pack()
+
+root.mainloop()
