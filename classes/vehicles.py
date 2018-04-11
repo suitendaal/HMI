3b9a9dcf00101353e131fd597e01befc7cb2dc46
suitendaal <sven.uitendaal1@gmail.com>
2018-04-11 11:48:14 +0200

    rotation amtrix

--- classes/vehicles.py
+++ classes/vehicles.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import math
+import numpy as np
 
 
 class Vehicle(object):
@@ -13,31 +13,37 @@ class Vehicle(object):
     def getCanvas(self, tk):
         center_x = self.position.pos_x
         center_y = self.position.pos_y
-        direction = self.position.direction * math.pi / 180
+        direction = np.radians(self.position.direction)
 
-        x0 = center_x - self.width / 2 * math.cos(direction) + self.length / 2 * math.sin(direction)
-        y0 = center_y - self.width / 2 * math.sin(direction) - self.length / 2 * math.cos(direction)
-        x1 = center_x + self.width / 2 * math.cos(direction) + self.length / 2 * math.sin(direction)
-        y1 = center_y + self.width / 2 * math.sin(direction) - self.length / 2 * math.cos(direction)
-        x2 = center_x + self.width / 2 * math.cos(direction) - self.length / 2 * math.sin(direction)
-        y2 = center_y + self.width / 2 * math.sin(direction) + self.length / 2 * math.cos(direction)
-        x3 = center_x - self.width / 2 * math.cos(direction) - self.length / 2 * math.sin(direction)
-        y3 = center_y - self.width / 2 * math.sin(direction) + self.length / 2 * math.cos(direction)
-        max_x = max(x0, x1, x2, x3)
-        min_x = min(x0, x1, x2, x3)
-        max_y = max(y0, y1, y2, y3)
-        min_y = min(y0, y1, y2, y3)
-        coord = x0, y0, x1, y1, x2, y2, x3, y3
-        canvas = Canvas(tk, bg=self.color, height=max_y - min_y, width=max_x - min_x)
-        canvas.create_polygon(coord)
-        return canvas
+        # x0 = center_x - self.width / 2 * math.cos(direction) + self.length / 2 * math.sin(direction)
+        # y0 = center_y - self.width / 2 * math.sin(direction) - self.length / 2 * math.cos(direction)
+        # x1 = center_x + self.width / 2 * math.cos(direction) + self.length / 2 * math.sin(direction)
+        # y1 = center_y + self.width / 2 * math.sin(direction) - self.length / 2 * math.cos(direction)
+        # x2 = center_x + self.width / 2 * math.cos(direction) - self.length / 2 * math.sin(direction)
+        # y2 = center_y + self.width / 2 * math.sin(direction) + self.length / 2 * math.cos(direction)
+        # x3 = center_x - self.width / 2 * math.cos(direction) - self.length / 2 * math.sin(direction)
+        # y3 = center_y - self.width / 2 * math.sin(direction) + self.length / 2 * math.cos(direction)
+        # max_x = max(x0, x1, x2, x3)
+        # min_x = min(x0, x1, x2, x3)
+        # max_y = max(y0, y1, y2, y3)
+        # min_y = min(y0, y1, y2, y3)
+        # coord = x0, y0, x1, y1, x2, y2, x3, y3
+        # canvas = Canvas(tk, bg=self.color, height=max_y - min_y, width=max_x - min_x)
+        # canvas.create_polygon(coord)
+        # return canvas
+        c, s = np.cos(direction), np.sin(direction)
+        matrixA = np.matrix('{} {}; {}, {}'.format(c, -s, s, c))
+        x0, y0 = np.array(np.dot(matrixA, [center_x - self.width / 2, center_y - self.length / 2]))[0]
+        print(np.array(v)[0])
+
+
 
 
 class Car(Vehicle):
 
     def __init__(self, position):
-        width = 50
-        length = 100
+        width = 5
+        length = 10
         color = 'red'
         super().__init__(position, width, length, color)
 
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from classes.hmidrawer import *
 from classes.vehicles import *
 from classes.position import *
+import matlab.engine
 
 
 hmi = HMIDrawer()
 
 position = Position(0, 0, 30)
 car = Car(position)
-canvas = car.getCanvas(hmi)
-canvas.pack()
+car.getCanvas(hmi)
+# canvas.pack()
 
-hmi.show()
+# hmi.show()
+
