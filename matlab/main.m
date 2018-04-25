0ab0a8dc3f333a98c3265aec56f82041ad28a303
suitendaal <sven.uitendaal1@gmail.com>
2018-04-25 13:49:18 +0200

    receiver werkt

--- matlab/main.m
+++ matlab/main.m
@@ -2,8 +2,8 @@ function [vehicle_positions, environment_positions, advisory_speed] = main()
 
     receiver = sim('receiver');
     data = receiver.get('simout');
-    vehicles = data[1];
-    environments = data[2];
+    vehicles = data(1);
+    environments = data(2);
 
     vehicle_positions = [];
     environment_positions = [];
