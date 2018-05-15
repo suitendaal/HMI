bf2450b14936c1afd13a971129ccb1836679885d
suitendaal <sven.uitendaal1@gmail.com>
2018-05-15 15:38:15 +0200

    statisch gat plotten in hmi gapplot

--- classes/speedprogram.py
+++ classes/speedprogram.py
@@ -82,7 +82,8 @@ class SpeedProgram(object):
         self.hmi.showError()
 
     def plotGap(self, gap):
-        self.hmi.plotGap(gap.rel_distance)
+        # self.hmi.plotGap(gap.rel_distance)
+        self.hmi.plotGap(0)
 
     def advisorySpeed(self):
         amount = 0
