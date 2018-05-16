1e38825052e60822ae4d5e31ce44827cada59611
suitendaal <sven.uitendaal1@gmail.com>
2018-05-16 14:45:28 +0200

    find out which error

--- udp/calculations.py
+++ udp/calculations.py
@@ -174,7 +174,7 @@ def checkIfError(old_vehicles, vehicles, old_gap, gap):
             if old_gap.vehicle_back != gap.vehicle_back or (old_gap.vehicle_back is not None and gap.vehicle_back is not
                                                             None and old_gap.vehicle_back.partnr !=
                                                             gap.vehicle_back.partnr):
-                print("gap changed")
+                #print("gap changed")
                 return True
 
     print("error false")
