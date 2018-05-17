9205cda1bf3f0a0e3c2d7bf98e9b074f444312af
suitendaal <sven.uitendaal1@gmail.com>
2018-05-17 13:41:40 +0200

    debug level

--- classes/hmidrawer.py
+++ classes/hmidrawer.py
@@ -27,6 +27,7 @@ class HMIDrawer(Tk):
 
         if level == 1:
             return
+        print("1")
 
         # Set right canvas.
         self.canvasright = Canvas(self.baseFrame, bg=colors['background'], highlightthickness=0)
