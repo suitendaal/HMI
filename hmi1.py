26c3ae6140e5cac3959b26ba127799f679f8c937
Jeffrey van der Zeeuw <jeffreyvanderzeeuw@hotmail.com>
2018-05-22 11:02:42 +0200

    setup

--- hmi1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def main():
-    os.system("py main.py 1")
-
-
-if __name__ == '__main__':
-    main()
---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+base = None
+
+executables = [Executable("main.py", base=base)]
+
+packages = ["idna", "numpy", "json", "os", "socket", "struct", "sys", "time", "tkinter"]
+options = {
+    'build_exe': {
+        'packages':packages,
+    },
+}
+
+setup(
+    name="HMI",
+    options=options,
+    version="1.0",
+    description="Choose between level 1 to 4 to get different levels of HMI supporting systems",
+    executables=executables
+)
