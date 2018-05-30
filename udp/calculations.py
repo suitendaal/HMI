a91db05040c539a525be68c1c3af038a7474b234
suitendaal <sven.uitendaal1@gmail.com>
2018-05-30 11:49:18 +0200

    size

--- udp/calculations.py
+++ udp/calculations.py
@@ -67,7 +67,7 @@ def findTargetGap(vehicle, gaps):
     # gaps.sort(key=lambda x: abs(x.disToInter() - vehicle.disToInter()))
     gaps = gaps[::-1]
 
-    print(gaps[0].xpos() - vehicle.position.xpos)
+    print(gaps[0].size())
 
     # Find nearest target gap which is at least the vehicle space.
     target_gap = None
